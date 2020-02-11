4b522589f74056a385801607363ce7d5ed3c56d0
Christoph Pfaffmann <christoph.pfaffmann@outlook.com>
2020-02-11 12:48:36 +0100

    added Darkmode

---
+++ js/toggle_slider.js
@@ -0,0 +1,20 @@
+var checkbox = document.querySelector("input[name=theme]");
+
+checkbox.addEventListener("change", function() {
+  if (this.checked) {
+    trans();
+    document.documentElement.setAttribute("data-theme", "dark");
+    document.documentElement.setAttribute("src", "images/Logo+Name_light.png");
+  } else {
+    trans();
+    document.documentElement.setAttribute("data-theme", "light");
+    document.documentElement.setAttribute("src", "images/Logo+Name_blue.png");
+  }
+});
+
+let trans = () => {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
